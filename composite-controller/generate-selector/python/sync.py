ab4d1b0c1c5c1ac01cccda729513214eae1fe587
Shovan Maity <shovan.cse91@gmail.com>
2020-03-30 02:51:57 +0530

    add steps for generate-selector (#6)
    add steps for generate-selector
    
    Signed-off-by: Shovan Maity <shovan.maity@mayadata.io>

--- composite-controller/generate-selector/python/sync.py
+++ composite-controller/generate-selector/python/sync.py
@@ -2,31 +2,46 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
 class Controller(BaseHTTPRequestHandler):
-  def sync(self, parent, children):
-    # Generate the desired child object.
-    name = parent.get("spec", {}).get("name","Unknown")
-    desired_ping_reply = [
-      {
-        "apiVersion": "example.com/v1",
-        "kind": "Pong",
-        "metadata": {
-          "name": parent["metadata"]["name"],
-          "labels": {
-            "name": parent["metadata"]["name"]
-          }
-        },
-        "spec": {
-          "message": "Hello %s" % name
-        }
-      }
-    ]
-
-    return {"children": desired_ping_reply}
 
   def do_POST(self):
-    # Serve the sync() function as a JSON webhook.
+
+    # Observed ping object
     observed = json.loads(self.rfile.read(int(self.headers.get("content-length"))))
-    desired = self.sync(observed["parent"], observed["children"])
+    ping = observed["parent"]
+    observed_pong_map = observed["children"].get("Pong.example.com/v1",{})
+
+    name = ping.get("spec", {}).get("name","Unknown")
+
+    pong = {
+      "apiVersion": "example.com/v1",
+      "kind": "Pong",
+      "metadata": {
+        "name": ping["metadata"]["name"],
+        "labels": {
+          "name": ping["metadata"]["name"]
+        }
+      },
+      "spec": {
+        "message": "Hello %s !!" % name
+      }
+    }
+
+    desired_children = []
+
+    # Comment out below 3 lines to try with other option
+    for name, observed_pong in observed_pong_map.items():
+      if name != ping["metadata"]["name"]:
+        desired_children.append(observed_pong)
+
+    desired_children.append(pong)
+
+    # Generate desired children
+    desired = {
+      "children": desired_children,
+      "status": {
+        "replicas": len(desired_children)
+      }
+    }
 
     self.send_response(200)
     self.send_header("Content-type", "application/json")
