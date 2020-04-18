4009b53cbbc6125633e7548f1d211cce63c6fffd
Shovan Maity <shovan.maity@mayadata.io>
2020-04-19 03:37:19 +0530

    add more example
    Signed-off-by: Shovan Maity <shovan.maity@mayadata.io>

---
+++ decorator-controller/sync-finalize-hook/python/finalize.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class Controller(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        observed = json.loads(self.rfile.read(
+            int(self.headers.get("content-length"))))
+        observed_pong_map = observed["attachments"].get(
+            "Pong.example.com/v1", {})
+
+        finalized = len(observed_pong_map) == 0
+        desired = {
+            "finalized": finalized
+        }
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(desired).encode())
+
+
+HTTPServer(("", 9090), Controller).serve_forever()
